b293fd895b38aa6ec9b0ddf63b39b968a3a74f9e
gael-boyenval <gaelboyenval@icloud.com>
2019-11-14 09:59:18 +0100

    feat: add sort option to MD

--- index.js
+++ index.js
@@ -1,22 +1,166 @@
-let postcss = require('postcss')
+const postcss = require('postcss')
+const list = require('postcss/lib/list')
 let startPacking = false
-let atRules = {}
+let queries = {}
+let queryLists = []
 
-function addToAtRules (atRule) {
-  const key = atRule.params
+/* eslint-disable */
 
-  if (!atRules[key]) {
-    atRules[key] = postcss.atRule({ name: atRule.name, params: atRule.params })
-  }
-  atRule.nodes.forEach(node => {
-    atRules[key].append(node.clone())
+const parseQueryList = queryList => {
+  const queries = []
+
+  list.comma(queryList).forEach(query => {
+    const expressions = {}
+
+    list.space(query).forEach(expression => {
+      let newExpression = expression.toLowerCase()
+
+      if (newExpression === 'and') {
+        return
+      }
+
+      if (/^\w+$/.test(newExpression)) {
+        expressions[newExpression] = true
+
+        return
+      }
+
+      newExpression = list.split(newExpression.replace(/^\(|\)$/g, ''), [':'])
+      const [feature, value] = newExpression
+
+      if (!expressions[feature]) {
+        expressions[feature] = []
+      }
+
+      expressions[feature].push(value)
+    })
+    queries.push(expressions)
   })
 
-  atRule.remove()
+  return queries
 }
 
-module.exports = postcss.plugin('postcss-media-queries-packer', () => root => {
-  root.walk(node => {
+const inspectLength = length => {
+  if (length === '0') {
+    return 0
+  }
+
+  const matches = /(-?\d*\.?\d+)(ch|em|ex|px|rem)/.exec(length)
+
+  if (!matches) {
+    return Number.MAX_VALUE
+  }
+
+  matches.shift()
+  const [num, unit] = matches
+  let newNum = num
+
+  switch (unit) {
+    case 'ch':
+      newNum = parseFloat(newNum) * 8.8984375
+
+      break
+
+    case 'em':
+    case 'rem':
+      newNum = parseFloat(newNum) * 16
+
+      break
+
+    case 'ex':
+      newNum = parseFloat(newNum) * 8.296875
+
+      break
+
+    case 'px':
+      newNum = parseFloat(newNum)
+
+      break
+  }
+
+  return newNum
+}
+
+const pickMinimumMinWidth = expressions => {
+  const minWidths = []
+
+  expressions.forEach(feature => {
+    let minWidth = feature['min-width']
+
+    if (!minWidth || feature.not || feature.print) {
+      minWidth = [null]
+    }
+
+    minWidths.push(minWidth.map(inspectLength).sort((a, b) => b - a)[0])
+  })
+
+  return minWidths.sort((a, b) => a - b)[0]
+}
+
+const sortQueryLists = (queryLists, sort) => {
+  const mapQueryLists = []
+
+  if (!sort) {
+    return queryLists
+  }
+
+  if (typeof sort === 'function') {
+    return queryLists.sort(sort)
+  }
+
+  queryLists.forEach(queryList => {
+    mapQueryLists.push(parseQueryList(queryList))
+  })
+
+  return mapQueryLists
+    .map((e, i) => ({
+      index: i,
+      value: pickMinimumMinWidth(e)
+    }))
+    .sort((a, b) => a.value - b.value)
+    .map(e => queryLists[e.index])
+}
+
+function addToAtRules (node) {
+  if (node.parent.parent && node.parent.parent.type !== "root") {
+    return;
+  }
+
+  if (node.parent.type !== "root") {
+    const newAtRule = postcss.atRule({
+      name: node.parent.name,
+      params: node.parent.params
+    });
+
+    node.each(rule => {
+      newAtRule.append(rule);
+    });
+    node.remove();
+    node.removeAll();
+    node.append(newAtRule);
+  }
+
+  const queryList = node.params;
+  const past = queries[queryList];
+
+  if (typeof past === "object") {
+    node.each(rule => {
+      past.append(rule.clone());
+    });
+  } else {
+    queries[queryList] = node.clone();
+    queryLists.push(queryList);
+  }
+
+  node.remove();
+}
+
+module.exports = postcss.plugin('postcss-media-queries-packer', options => root => {
+  const opts = {
+    sort: false,
+    ...options
+  };
+  root.each(node => {
     if (node.type === 'comment' && node.text === 'mqp:start') {
       startPacking = true
       node.remove()
@@ -32,12 +176,17 @@ module.exports = postcss.plugin('postcss-media-queries-packer', () => root => {
 
     if (
       node.type === 'comment' &&
-        node.text === 'mqp:end' &&
-        startPacking === false
+      node.text === 'mqp:end' &&
+      startPacking === false
     ) {
-      Object.keys(atRules).forEach(key => node.before(atRules[key]))
+
+      sortQueryLists(queryLists, opts.sort).forEach(queryList => {
+        node.before(queries[queryList]);
+      });
+
       node.remove()
-      atRules = {}
+      queries = {}
+      queryLists = []
     }
   })
 })
--- index.test.js
+++ index.test.js
@@ -4,6 +4,8 @@ let plugin = require('./')
 
 async function run (input, output, opts) {
   let result = await postcss([plugin(opts)]).process(input, { from: undefined })
+  console.log(result.css)
+
   expect(result.css).toEqual(output)
   expect(result.warnings()).toHaveLength(0)
 }
@@ -12,34 +14,34 @@ it('regroup media queries between comments', async () => {
   await run(
 `/* mqp:start */
 .tata {
-  color: blue
+  color: blue;
 }
 @media (min-width: 1024px) {
   .tata {
-    color: blue
+    color: blue;
   }
 }
 .toto {
-  color: blue
+  color: blue;
 }
 @media (min-width: 1024px) {
   .toto {
-    color: blue
+    color: blue;
   }
 }
 /* mqp:end */`,
 `.tata {
-  color: blue
+  color: blue;
 }
 .toto {
-  color: blue
+  color: blue;
 }
 @media (min-width: 1024px) {
   .tata {
-    color: blue
+    color: blue;
   }
   .toto {
-    color: blue
+    color: blue;
   }
 }`, {})
 })
@@ -167,11 +169,29 @@ it('should restart group after finding a new start comment', async () => {
 `.tata {
   color: blue
 }
-@media (min-width: 320px);
-@media (min-width: 1024px);
+@media (min-width: 320px) {}
+@media (min-width: 1024px) {}
 .tata {
   color: blue
 }
-@media (min-width: 320px);
-@media (min-width: 1024px)`, {})
+@media (min-width: 320px) {}
+@media (min-width: 1024px) {}`, {})
+})
+
+it('should sort media queries by size', async () => {
+  await run(
+`/* mqp:start */
+.tata {
+  color: blue
+}
+@media (min-width: 680px) {}
+@media (min-width: 320px) {}
+@media (min-width: 1024px) {}
+/* mqp:end */`,
+`.tata {
+  color: blue
+}
+@media (min-width: 320px) {}
+@media (min-width: 680px) {}
+@media (min-width: 1024px) {}`, { sort: true })
 })
